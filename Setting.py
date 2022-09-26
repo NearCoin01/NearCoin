bd9c598b4be40270217ecfb2ae28fe64825786d0
NearCoin01 <114406166+NearCoin01@users.noreply.github.com>
2022-09-26 20:59:52 +0900

    Add files via upload

---
+++ Setting.py
@@ -0,0 +1,15 @@
+# 웹훅 적으세여
+
+관리자=[1023018928374558781] #관리자 id
+서버이름="Near Coin"
+입출금로그= # 입출금로그 채널 id
+입출금로그웹훅="https://discordapp.com/api/webhooks/1023028768966717510/7lOsbWRzQ1ANdrtCo5nxaBrduW8RRD49yff8Iu8i-vJXKfN14N8GZ405IwHNtFHy9qEN" # 입출금로그 채널 웹훅
+계좌번호="토스뱅크 1908 5332 1429 조연우" # 은행명 계좌번호 예금주
+충전채널=1023029051427930183
+요청채널=1023029104515223613
+코인채널=1023031002458755125
+컬쳐계정=""
+컬쳐비번=""
+api토큰=""
+
+봇토큰="MTAyMzIxNjYwMDc4ODI0NjU4MA.GOG-ax.bbsNsALYESR4V0wQ6wCReh3DcRfSQyOVDdj"
